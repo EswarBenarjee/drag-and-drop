7d0f4adf165f60cdb667ce0c090c610a18423cb2
EswarBenarjee <eswar.bane2002.india@gmail.com>
2023-09-29 23:04:09 +0530

    chore: removing shallowcopy

--- src/store/Datastore.js
+++ src/store/Datastore.js
@@ -1,22 +1,5 @@
 import { create } from "zustand";
 
-const shallowEqual = (obj1, obj2) => {
-  const keys1 = Object.keys(obj1);
-  const keys2 = Object.keys(obj2);
-
-  if (keys1.length !== keys2.length) {
-    return false;
-  }
-
-  for (let key of keys1) {
-    if (!obj2.hasOwnProperty(key) || obj1[key] !== obj2[key]) {
-      return false;
-    }
-  }
-
-  return true;
-};
-
 const useStore = create((set) => ({
   boxes: [
     {
